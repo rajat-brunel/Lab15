a91649afeca6797b37476ac3e05889fa4363dfd4
Rajat Awasthi <r.awa3124@gmail.com>
2017-04-23 12:05:09 +0100

    Completed the Comments

--- src/Efficiency.java
+++ src/Efficiency.java
@@ -3,62 +3,114 @@ import java.util.ArrayList;
 
 public class Efficiency {
 	
+	/**
+	 * MST of a Dataset
+	 * @return MST of a particular Dataset
+	 */
 	private static double[][] MST() {
+		
+		//Number of Cities
 		int n=Algorithms.cities;
+		//Location of the Dataset
 		String filename=Functions.file(n);
+		//Read in the dataset
 		double dist[][]=TSP.ReadArrayFile(filename, " ");
+		//Get MST in 2d array through Prims Algorithm using the dataset
 		double[][] mstOfArray = MST.PrimsMST(dist);
 		return mstOfArray;
 	}
 	
+	/**
+	 * Fitness of the MST
+	 * @param MST_Array A MST Array
+	 * @return Fitness of the MST Array Provided
+	 */
 	private static double MST_Fitness(double[][] MST_Array) {
 		
+		//Initialise the Fitness
 		double fitness = 0.0;
+		
+		//As MST is a 2d Array we need a Nested loop
+		//It runs until the length of the MST
 		for (int i = 0; i < MST_Array.length; i++) {
 			for (int j = 0; j < MST_Array[i].length; j++) {
+				
+				//Adds the fitness from the values of each row and column
 				fitness = fitness + MST_Array[i][j];
 			}
 		}
+		//Divide fitness by 2 as MST is a 2D array
 		fitness = fitness / 2;
 		return fitness;
 	}
-
+	
+	/**
+	 * Calculates the Efficiency of Fitness against MST
+	 * @param fitness Fitness of the Algorithm
+	 * @return Efficency of the Algorithm against MST
+	 */
 	public static double MST_Efficiency(double fitness) {
+		
+		//Get the MST Fitness
 		double[][] array = MST();
 		double mst_fitness = MST_Fitness(array);
 		
+		//Calculate Efficiency
 		double efficiency = (mst_fitness / fitness) * 100;
 
 		return efficiency;
 	}
+	/**
+	 * Calculates The Efficiency Against the Optimal Tour
+	 * @param fitness Fitness From The Algorithm
+	 * @return Efficiency Against the Optimal Tour
+	 */
 	public static double OPT_Efficiency(double fitness){
 		int n=Algorithms.cities;
 		double opt=OPT_fitness(n);
 		double efficiency=(opt/fitness)*100;
 		return efficiency;
 	}
-	
+	/**
+	 * Gets the Fitness of the Optimal Tour 
+	 * @param n Number of Cities
+	 * @return Fitness of the Optimal Tour
+	 */
 	public static double OPT_fitness(int n){
 		
+		//Declare a new ArrayList to store the Optimal Tour
 		ArrayList<Integer> t = new ArrayList<Integer>();
 		
+		//Convert the integer value of cities into String Data Type
 		String cities= String.valueOf(n);
 		
+		//Get the location the required OPT File
 		String filename=Functions.OPT_file(n);
 		
+		//Add the OPT to the Array List
 		t=TSP.ReadIntegerFile(filename);
 		
+		//Gets the Fitness of the Optimal Tour
 		double dist=Functions.distance(t);
 		return dist;
 	}
 	
+	/**
+	 * Efficiency Percentage
+	 * @param fitness Fitness of the Algorithm
+	 * @return Effciency in a 2 decimal format
+	 */
 	public static String percentage(double fitness){
-		
+			
+		//Initialise the Format to use
 		 DecimalFormat f = new DecimalFormat("##.00");
 		
+		 //Gets the MST Efficiency
 		double mst_eff=Efficiency.MST_Efficiency(fitness);
+		//Initialise the OPT Efficiency
 		double opt_eff=0.0;
 		
+		// If OPT Tour Available get the Efficiency of the OPT Tour
 		if (Algorithms.cities==48||Algorithms.cities==51||
 				Algorithms.cities==52||Algorithms.cities==70||
 				Algorithms.cities==76||Algorithms.cities==100||
@@ -67,6 +119,7 @@ public class Efficiency {
 		opt_eff=Efficiency.OPT_Efficiency(fitness);
 		
 		}
+		
 		String eff=" "+f.format(opt_eff)+" " + f.format(mst_eff);
 		System.out.println(eff);
 		return eff;
--- src/Functions.java
+++ src/Functions.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 
 public class Functions {
 	
+	 /**
+	  * Dataset 
+	  * @param cities Number of Cities
+	  * @return Dataset of distance between the Cities
+	  */
 	 public static double[][] dataset(int cities) {
 		
 		double dist[][];
@@ -12,7 +17,12 @@ public class Functions {
 		dist=TSP.ReadArrayFile(filename, " ");
 		return dist;
 	}
-	 
+	
+	 /**
+	  * File Path
+	  * @param cities Number of Cities
+	  * @return Location of the File
+	  */
 	 public static String file(int cities){
 		 String path="dataset/";
 		 String start="TSP_";
@@ -21,7 +31,11 @@ public class Functions {
 		 String locat=path+start+cities+end;
 		 return locat;
 	 }
-	 
+	 /**
+	  * OPT File Path
+	  * @param cities Number of Cities
+	  * @return Location of the OPT File
+	  */
 	 public static String OPT_file(int cities){
 		 String path="dataset/";
 		 String start="TSP_";
@@ -32,25 +46,34 @@ public class Functions {
 		 return locat;
 	 }
 	
-	public static ArrayList<Integer> swap(ArrayList<Integer> r_tour){
+	/**
+	 * The Small Change Method
+	 * It swaps two cities
+	 * @param r_tour A tour
+	 * @return The Swapped City Tour
+	 */
+	 public static ArrayList<Integer> swap(ArrayList<Integer> r_tour){
 			
-			int i=0;
-			int j=0;
+		int i=0;
+		int j=0;
+		
+		//Declare the Changed Tour
+		ArrayList<Integer> changed_tour = new ArrayList<Integer>();
+		
+		//While loop until we get two different random numbers
+		//Uses CS2004 Class, gets random numbers from 0 to the length of the tour
+		while(i==j){
+					i= CS2004.UI(0,r_tour.size()-1);
+					j= CS2004.UI(0,r_tour.size()-1);
+				}
 			
-			ArrayList<Integer> changed_tour = new ArrayList<Integer>();
-			
-			while(i==j){
-				i= CS2004.UI(0,r_tour.size()-1);
-				j= CS2004.UI(0,r_tour.size()-1);
-			}
-			
-		//	System.out.println(i + " " + j);
-			
-			for (int a=0;a<Algorithms.cities;a++){
+		//Populates the Changed tour with all the Cities from the original Tour
+		for (int a=0;a<Algorithms.cities;a++){
 				
-				changed_tour.add(r_tour.get(a));
-			}
+			changed_tour.add(r_tour.get(a));
 			
+			}
+			//Swaps the Cities using the index values from earlier
 			changed_tour.set(i,r_tour.get(j));
 			changed_tour.set(j,r_tour.get(i));
 			
@@ -59,6 +82,11 @@ public class Functions {
 			return changed_tour;
 		}
 	
+	/**
+	 * Distance of a Tour (Fitness)
+	 * @param t A Tour
+	 * @return The Fitness of the Tour
+	 */
 	public static double distance(ArrayList<Integer> t){
 				
 				//Initialise variable for the Final length of the tour
@@ -77,7 +105,6 @@ public class Functions {
 					//Adding the sum from previous cities
 					s= s + Algorithms.distanceArray[a][b];
 					
-				//	System.out.println("dist " + a + " "+ b +" = " + dist[a][b]);
 				} 
 				
 				//As we have to return to our starting city
@@ -86,42 +113,56 @@ public class Functions {
 				//get the city at the start
 				int start_city = t.get(0);
 				
-			//	System.out.println("dist " + end_city + " "+ start_city +" = " + dist[end_city][start_city]);
 				
 				//Add the distance between the start city to the end city 
 				s= s+Algorithms.distanceArray[end_city][start_city];
 				
 				
-				//Output the total distance 
-				
-				
-			//	System.out.println("Length of the tour is  : "+   s); 
+				//Output the total distance 	
+				//System.out.println("Length of the tour is  : "+   s); 
 				
 				//We need to minimise this value
 				return s;
 	}
 	
+	/**
+	 * Makes a Tour
+	 * @return A New Random Tour
+	 */
 	public static ArrayList<Integer> tour(){
+		
+		//Initialise two Array Lists
+		//1 to add all the values in linear order
+		//2 to randomise the values and get a random tour
 		ArrayList<Integer> p = new ArrayList<Integer>();
 		
 		ArrayList<Integer> t = new ArrayList<Integer>(); 
 		
+		
+		//Use a for loop to add integers according to the number of cities
 		for (int i=0;i<Algorithms.cities;i++){
 			p.add(i);
 		}
 		int i;
 		
+		//Runs a while loop until the original Linear Tour is empty
 		while(p.size()>0){
 			
+			//get a random integer from the original tour
 			i=CS2004.UI(0,p.size());
+			
+			//if the random integer is the last integer in the array
+			//get a random integer again
+			//As the length would be 1 more than the actual tour cities (0-47) for 48 cities
 			do{
 				i=CS2004.UI(0,p.size());
 			}while
 				(p.size()<i || p.size()==i);
 		
-		
 			int add=p.get(i);
+			//Remove the City from the Linear Tour
 			t.add(add);
+			//Add the City to the new Random Tour
 			p.remove(i);
 			
 		}
@@ -129,16 +170,24 @@ public class Functions {
 		return t;
 	
 	}
-	 
+	 /**
+	  * Writes the Results to a Text File in CSV
+	  * @param fitness fitness of the Algorithm
+	  * @param efficiency efficiencies of the Algorithm
+	  * @param algorithm Name of the Algorithm
+	  * @param append Overwrite or Create new file
+	  */
 	public static void Results(double fitness, String efficiency, String algorithm, boolean append){
 		
+		//Converts the fitness from Double to String Data Type
 		String fitness_string= Double.toString(fitness);
+		//Creates the filename in a proper readable format
 		String filename="results/"+algorithm+"_"+Algorithms.cities+"_"+Algorithms.iter+".txt";
 		
 		try {
-
+			//Creates the file
 		      File file = new File(filename);
-
+		      // Check if the file is already present
 		      if (file.createNewFile()){
 		        System.out.println("File is created!");
 		      }else{
@@ -149,6 +198,7 @@ public class Functions {
 		      e.printStackTrace();
 	    	}
 		
+		// Writes the required results into the file
 		try{
 			FileWriter writer = new FileWriter(filename, append);
 			writer.write(fitness_string+" "+efficiency);
